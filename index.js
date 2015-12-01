f3b82a63e48bd0e8ee55584716c92668fbe9ce44
wandergeek <innernetz@gmail.com>
2015-12-01 15:12:09 -0700

    changed message

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var client = redis.createClient('6379', 'redis');
 app.get('/', function(req, res, next) {
   client.incr('counter', function(err, counter) {
     if(err) return next(err);
-    res.send('This page has been viewed ' + counter + ' times, guy');
+    res.send('This page has been viewed ' + counter + ' times!!!!');
   });
 });
 
